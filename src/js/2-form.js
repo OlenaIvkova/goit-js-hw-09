606af69b43a6a9c7779d622cef7dc50fb55fbb33
Olena <alchimeraa@gmail.com>
2024-09-09 03:45:16 +0300

    js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,11 +1,39 @@
 // console.log(`Form`);
 
-// Описаний в документації
-import SimpleLightbox from "simplelightbox";
+let formData = {
+    email: "",
+    message: ""
+};
 
-// Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css";
+const form = document.querySelector('.feedback-form');
 
-const SimpleLightbox('.form a', { /* options */ });
+form.addEventListener('input', (event) => {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
 
-console.log(SimpleLightbox);
+document.addEventListener('DOMContentLoaded', () => {
+    const savedData = localStorage.getItem('feedback-form-state');
+
+    if (savedData) {
+        formData = JSON.parse(savedData);
+        form.elements.email.value = formData.email;
+        form.elements.message.value = formData.message;
+    }
+});
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (!formData.email || !formData.message) {
+        alert('Fill please all fields');
+        return;
+    }
+
+    console.log(formData);
+
+    form.reset();
+    localStorage.removeItem('feedback-form-state');
+    formData = { email: "", message: "" };
+});
+
