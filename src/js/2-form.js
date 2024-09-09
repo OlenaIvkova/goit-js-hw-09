c69e4f4df057dde298476651eb206e11fe218059
Olena <alchimeraa@gmail.com>
2024-09-09 03:02:30 +0300

    js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,11 @@
-console.log(`Form`);
+// console.log(`Form`);
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const SimpleLightbox('.form a', { /* options */ });
+
+console.log(SimpleLightbox);
